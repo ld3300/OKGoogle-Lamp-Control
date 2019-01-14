b1fac639566979ee494135b9cf996f0da99bf070
David Smith <davidosmith@gmail.com>
2019-01-13 19:50:24 -0500

    add template for private.h

---
+++ include/private_template.h
@@ -0,0 +1,6 @@
+// Template file. Rename to private.h to avoid commiting to github
+
+#define MQTT_SERV     "io.adafruit.com"  // or other MQTT service
+#define MQTT_PORT     1883
+#define MQTT_NAME     "YOUR USERNAME"
+#define MQTT_PASS     "YOUR AIO KEY"
