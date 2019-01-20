76b2b15e5bcc2fc895180be0d55350979e2e7f36
David Smith <davidosmith@gmail.com>
2019-01-20 01:39:02 -0500

    add check in loop to reconnect if wifi lost.  added lots of comments.

--- include/private_template.h
+++ include/private_template.h
@@ -1,8 +1,11 @@
+
 // Template file. Rename to private.h to avoid commiting to github
 
-#define MQTT_SERV     "io.adafruit.com"  // or other MQTT service
-#define MQTT_PORT     1883
-#define MQTT_NAME     "YOUR USERNAME"
-#define MQTT_PASS     "YOUR AIO KEY"
+#define AIO_SERVER      "io.adafruit.com"       // or other MQTT service
+#define AIO_SERVERPORT  1883                    // 1883 or 8883 for SSL security
+#define AIO_CLIENTID    "YOUR MQTT CLIENT ID"   // Client ID for this device, must be unique 1-23 chars and improves successful connections
+#define AIO_USERNAME    "YOUR USERNAME"         // Adafruit IO username
+#define AIO_KEY         "YOUR AIO KEY"          // AIO Key, available online dashboard
 
-#define OTA_PASS      "OTA PASSWORD"
+#define HOSTNAME        "MyWifiHostname"        // Hostname for AP
+#define OTA_PASS        "OTA PASSWORD"          // Password for OTA updates, I couldn't get it to work
--- src/main.cpp
+++ src/main.cpp
@@ -25,25 +25,25 @@
 #include <ArduinoOTA.h>
 #include "Adafruit_MQTT.h"
 #include "Adafruit_MQTT_Client.h"
-#include "private.h"
+#include "private.h"                      // place to store passwords and keys.  add private.h to .gitignore
 
-#define PUSHUPDATETIME 3600000  // 60 minutes
+#define PUSHUPDATETIME 3600000            // 60 minutes -- How often to push current state to server to maintain sync
 
 #define LAMPPIN 0                         // Pin controlling lamp output
 #define SWITCHPIN 12                      // Pin that reads change of built-in lamp switch (soldered to chip on esp-01)
-#define LAMPON "lampon"
+#define LAMPON "lampon"                   // strings to look for in on and off functions
 #define LAMPOFF "lampoff"
-#define STATUSREQUEST "lampstatus"
-#define AIOTHROTTLETIMEOUT 60000    // time to wait before trying to publish again if we hit throttle status
+#define STATUSREQUEST "lampstatus"        // we will use this string to identify when someone wants us to publish current on/off state
+#define AIOTHROTTLETIMEOUT 60000          // time to wait before trying to publish again if we hit throttle status
 
 const IPAddress apIP(192, 168, 1, 1);     // IP when in AP mode (when wifi connect fails)
 const char *apSSID = "Bedroom_Lamp_SETUP";     // AP name to connect to to configure wifi
-bool settingMode;                      // Store setting of wifi connect
+bool settingMode;                         // Store setting of wifi connect
 String ssidList;                          // Scanned SSIDs in AP mode
-bool lampState = false;                    // Is lamp currently on or off
+bool lampState = false;                   // Is lamp currently on or off
 bool lastLampState = false;
-unsigned long lastUpdateTime = 0;
-bool mqtt_publish_overflow = false;
+unsigned long lastUpdateTime = 0;         // Last time we sent update so we don't do it too often
+bool mqtt_publish_overflow = false;       // flag for if Adafruit IO data rate exceeded
 
 DNSServer dnsServer;
 ESP8266WebServer webServer(80);
@@ -51,13 +51,16 @@ ESP8266WebServer webServer(80);
 // Create an ESP8266 WiFiClient class to connect to the MQTT server.
 WiFiClient client;
 // or... use WiFiClientSecure for SSL
-//WiFiClientSecure client;
+// WiFiClientSecure client;
 
-Adafruit_MQTT_Client mqtt(&client, MQTT_SERV, MQTT_PORT, MQTT_NAME, MQTT_PASS);   // These values are stored in private.h file
-Adafruit_MQTT_Subscribe bedroom = Adafruit_MQTT_Subscribe(&mqtt, MQTT_NAME "/feeds/bedroom", MQTT_QOS_1);
-Adafruit_MQTT_Publish bedroomPub = Adafruit_MQTT_Publish(&mqtt, MQTT_NAME "/feeds/bedroom", MQTT_QOS_1);
-Adafruit_MQTT_Publish bedroomPush = Adafruit_MQTT_Publish(&mqtt, MQTT_NAME "/feeds/bedroomStatus", MQTT_QOS_1);
-Adafruit_MQTT_Subscribe throttle = Adafruit_MQTT_Subscribe(&mqtt, MQTT_NAME "/throttle", MQTT_QOS_1);  // check if data limit reached
+// setup for publishing and subscribing.  AIO feeds are username/feeds/feedname or username/f/feedname
+// With the exception of some like username/throttle or username/errors or time/seconds
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_CLIENTID, AIO_USERNAME, AIO_KEY);   // These values are stored in private.h file
+Adafruit_MQTT_Publish bedroomPush = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/bedroomStatus", MQTT_QOS_1);
+Adafruit_MQTT_Publish bedroomPub = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/bedroom", MQTT_QOS_1);
+Adafruit_MQTT_Subscribe bedroom = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/bedroom", MQTT_QOS_1);
+Adafruit_MQTT_Subscribe throttle = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/throttle", MQTT_QOS_1);  // check if data limit reached
+// Adafruit_MQTT_Subscribe timefeed = Adafruit_MQTT_Subscribe(&mqtt, "time/seconds"); // In case want to do time based actions
 
 void MQTT_connect(){
   int8_t ret;
@@ -72,13 +75,12 @@ void MQTT_connect(){
     Serial.println(mqtt.connectErrorString(ret));
     Serial.println("Retrying MQTT connection in 5 seconds...");
     mqtt.disconnect();
-    delay(5000); // wait 5 seconds
+    delay(2000); // wait 5 seconds
     retries--;
-    if (retries == 0)
+    if (retries == 0) // if we run out of tries do rest of loop
     {
-      // basically die and wait for WDT to reset me
-      while (1)
-        ;
+      // hold, we can loop and try again later
+      delay(3000);
     }
   }
   Serial.println("MQTT Connected!");
@@ -110,7 +112,7 @@ void ota_setup(){
   // OTA Port defaults to 8266
   // ArduinoOTA.setPort(8266);
   // Hostname defaults to esp8266-[ChipID]
-  ArduinoOTA.setHostname("BedroomLamp");
+  ArduinoOTA.setHostname(HOSTNAME);                     // defined in private.h
   // No authentication by default
   // ArduinoOTA.setPassword((const char *)OTA_PASS);
 
@@ -138,9 +140,9 @@ void lampStatePublish(bool xlampstate){       // We are going to publish our sta
   Serial.print(F("\nSending lamp val "));
   Serial.print(xlampstate);
   Serial.print("...");
-  char* xlamp = LAMPOFF;
-  if(xlampstate) xlamp = LAMPON;
-  if (! bedroomPush.publish(xlamp)) {
+  char* xlamp = LAMPOFF;                    // initialize to off
+  if(xlampstate) xlamp = LAMPON;            // if our state is on
+  if (! bedroomPush.publish(xlamp)) {       // return this value so controllers can update
     Serial.println(F("Failed"));
   } 
   else {
@@ -149,88 +151,100 @@ void lampStatePublish(bool xlampstate){       // We are going to publish our sta
 }
 
 void update_state(bool updateRequest) {
+  // if we get an update request, or if lamp state changed, or if we hit our interval update timeout
   if(updateRequest || lastLampState != lampState || (millis() - lastUpdateTime > PUSHUPDATETIME)){
-    static unsigned long mqtt_last_time = millis();
-    static bool overflow_flag = false;
-    if(mqtt_publish_overflow){                  // Stop publishing if we have hit service overflow
-      mqtt_last_time = millis();
-      overflow_flag = true;
-      mqtt_publish_overflow = false;
+    static unsigned long flag_last_time = millis();   // Last time we saw AIO throttle flag
+    static bool overflow_flag = false;                // flag to keep us from attempting to write while throttled
+    if(mqtt_publish_overflow){                        // Stop publishing if we have hit service overflow
+      flag_last_time = millis();                      // We saw a throttle, update flag time
+      overflow_flag = true;                           // activate throttle flag
+      mqtt_publish_overflow = false;                  // clear overflow so we can get updated if it happens again before timeout
     }
-    else if (!overflow_flag || (millis() - mqtt_last_time > AIOTHROTTLETIMEOUT)){
-      lampStatePublish(lampState);                // Push state change back to MQTT server
-      lastLampState = lampState;
-      lastUpdateTime = millis();
-      overflow_flag = false;
+    // if there is no throttle flag, or if we hit the throttle timeout
+    else if (!overflow_flag || (millis() - flag_last_time > AIOTHROTTLETIMEOUT)){
+      lampStatePublish(lampState);                    // Push state change back to MQTT server
+      lastLampState = lampState;                      // update last lamp to current state
+      lastUpdateTime = millis();                      // record last update time so we don't overpublish
+      overflow_flag = false;                          // clear overflow flag if it was set
     }
   }
 }
 
-void handleSwitchISR() {
-  static unsigned long last_interrupt_time = 0;
-  unsigned long interrupt_time = millis();
-  if(interrupt_time - last_interrupt_time > 200){
-    lampState = !lampState;                     // update lamp state
-    digitalWrite(LAMPPIN, lampState);          // Invert state of lamp
-    last_interrupt_time = interrupt_time;
+void handleSwitchISR() {    // ISR for switch with debounce.  Could be handled in a less process intense way
+  static unsigned long last_interrupt_time = 0;   // keep the last time interrupt called
+  unsigned long interrupt_time = millis();        // get current time
+  if(interrupt_time - last_interrupt_time > 200){ // If ISR gets calls more often that 200ms then it is bounce
+    lampState = !lampState;                       // update lamp state
+    digitalWrite(LAMPPIN, lampState);             // Invert state of lamp
+    last_interrupt_time = interrupt_time;         // Set last interrupt time to block button bounce
   }
+}
+
+void readSubscriptions(){
+  Adafruit_MQTT_Subscribe *subscription;
+  while ((subscription = mqtt.readSubscription(500))){
+    //If we're in here, a subscription updated...
+    if (subscription == &bedroom){            // check if incoming is for bedroom feed
+      char buffer[50];
+      sprintf(buffer, "%s", (char *)bedroom.lastread);
+      //Print the new value to the serial monitor
+      Serial.print("Bedroom: ");
+      Serial.println(buffer);                 // print the entire buffer of bedroom feed
+      if (strstr(buffer, LAMPON) != NULL){    // if we get lamp on command
+        digitalWrite(LAMPPIN, HIGH);          // pull high
+        Serial.println("On executed");
+        lampState = true;                     // update lamp state
+      }
+      else if (strstr(buffer, LAMPOFF) != NULL){    // if lamp off command, write lamp pin low and change state
+        digitalWrite(LAMPPIN, LOW);
+        Serial.println("Off executed");
+        lampState = false;
+      }
+      else if (strstr(buffer, STATUSREQUEST) != NULL){  // If we get a status request then force state update
+        update_state(true);
+      }
+    }
+    else if (subscription == &throttle){    // if the throttle feed activated then we have a publishing overflow
+      mqtt_publish_overflow = true;         // the maximum publish rate is shared among all devices on account
+    }
+  }
+
 }
 
 void setup(){
-  pinMode(LAMPPIN, OUTPUT);
-  pinMode(SWITCHPIN, INPUT);
-  Serial.begin(115200);
+  pinMode(LAMPPIN, OUTPUT);           // Pin used to drive lamp controlling circuit see README for info
+  pinMode(SWITCHPIN, INPUT);          // Pin we put in line switch on
+  attachInterrupt(SWITCHPIN, handleSwitchISR, CHANGE);  // declare ISR first so that switch will work if wifi hangs
 
-  attachInterrupt(SWITCHPIN, handleSwitchISR, CHANGE);
+  Serial.begin(115200);
   
-  wifi_setup();
-  ota_setup();
+  wifi_setup();                     // Setup and connect to wifi
+  ota_setup();                      // Setup for OTA
 
   mqtt.subscribe(&bedroom);         // Subscribe to the bedroom feed on adafruit io
-  mqtt.subscribe(&throttle);
-  MQTT_connect();
+  mqtt.subscribe(&throttle);        // throttle feed will help us if we publish too frequently
+  MQTT_connect();                   // Connect to server so we can send initial state
 
   bedroomPub.publish(LAMPOFF);      // write an initial state to keep things intact until ability to get last message is available
   lampStatePublish(false);
 }
 
 void loop(){
+  //connect wifi if not connected
+  if (WiFi.status() != WL_CONNECTED) {
+    delay(1);
+    wifi_setup();
+  }
+
   ArduinoOTA.handle();          // Check for OTA
-  //Connect/Reconnect to MQTT
-  MQTT_connect();
+  
+  MQTT_connect();               // Connect/Reconnect to MQTT
 
   // periodically push state to mqtt. either by state change, timeout, or millis() rollover
-  update_state(false);
+  update_state(false);          // the boolean is used to satisfy an external update request
 
-  // Read from our subscription queue until we run out, or
-  // wait up to 5 seconds for subscription to update
-  Adafruit_MQTT_Subscribe *subscription;
-  while ((subscription = mqtt.readSubscription(500))){
-    //If we're in here, a subscription updated...
-    if (subscription == &bedroom){
-      char buffer[50];
-      sprintf(buffer, "%s", (char *)bedroom.lastread);
-      //Print the new value to the serial monitor
-      Serial.print("Bedroom: ");
-      Serial.println(buffer);
-      if (strstr(buffer, LAMPON) != NULL){
-        digitalWrite(LAMPPIN, HIGH);
-        Serial.println("On executed");
-        lampState = true;
-      }
-      else if (strstr(buffer, LAMPOFF) != NULL){
-        digitalWrite(LAMPPIN, LOW);
-        Serial.println("Off executed");
-        lampState = false;
-      }
-      else if (strstr(buffer, STATUSREQUEST) != NULL){
-        update_state(true);
-      }
-    }
-    else if (subscription == &throttle){
-      mqtt_publish_overflow = true;
-    }
-  }
+  // Read from our subscription queue until we run out
+  readSubscriptions();
 
   // ping the server to keep the  mqtt connection alive
   // if (!mqtt.ping()){             // Commented because it seemed to be causing packet issues
